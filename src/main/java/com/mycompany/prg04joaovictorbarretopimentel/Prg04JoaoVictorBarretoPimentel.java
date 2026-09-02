55e801f4ce95d74f3d946c1006115b024cf7b445
20252ireads0018-netizen <20252ireads0018@ifba.edu.br>
2026-09-02 17:46:49 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/prg04joaovictorbarretopimentel/Prg04JoaoVictorBarretoPimentel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg04joaovictorbarretopimentel;
+
+/**
+ *
+ * @author xexpl
+ */
+public class Prg04JoaoVictorBarretoPimentel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
